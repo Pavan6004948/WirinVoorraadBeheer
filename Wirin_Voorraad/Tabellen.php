6fed1bcca52007a81a765b2b0b1bda2c677b7cff
Pavan6004948 <6004948@mborijnland.nl>
2023-03-02 09:43:40 +0100

    tabellen

--- Wirin_Voorraad/Tabellen.php
+++ Wirin_Voorraad/Tabellen.php
@@ -4,3 +4,13 @@ require 'Navigatie.php';
 require 'Voorraad.php';
 
 ?>
+
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+</table>
