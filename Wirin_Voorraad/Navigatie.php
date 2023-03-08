3097f77d9c9d9ff0338e3ee4f42de6e3786cb845
Pavan6004948 <6004948@mborijnland.nl>
2023-03-08 12:45:52 +0100

    wirin

--- Wirin_Voorraad/Navigatie.php
+++ Wirin_Voorraad/Navigatie.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Winin_Voorraad</title>
+    <title>Wirin_Voorraad</title>
 
     <link rel="stylesheet" href="./style/Wirin_Css.css">
     <link rel="stylesheet" href="https://kit.fontawesome.com/1ac00105be.css" crossorigin="anonymous">
